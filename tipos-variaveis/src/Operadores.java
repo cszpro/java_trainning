bf66d7718bc8ca32578312b9bb3e535dec9081d6
cszpro <sz8.celio@gmail.com>
2024-12-13 09:44:28 -0300

    exercitando comparar strings

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -9,5 +9,16 @@ public class Operadores {
 
         String resultado = a==b ? "Verdadeiro" : "Falso";
         System.out.println(resultado);
-}
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println(simNao);
+
+        String nomeUm = "Gleyson";
+        String nomeDois = new String("Gleyson");
+        System.out.println(nomeUm.equals(nomeDois));
+
+    }
 }
