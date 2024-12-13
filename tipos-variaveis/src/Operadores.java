4082513c0eb810bed32d6664d91866b8f72da194
cszpro <sz8.celio@gmail.com>
2024-12-13 09:30:43 -0300

    testando booleanos e concatenação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,5 +2,12 @@ public class Operadores {
     public static void main(String[] args) {
         String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
         System.out.println(nomeCompleto);
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+        System.out.println(resultado);
 }
 }
